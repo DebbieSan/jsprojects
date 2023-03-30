ed2e2ab68d7e47e9b033e9d8b008ed3ee72e1e20
debbiesan <debbie.san16@gmail.com>
2023-03-29 18:11:26 -0600

    Initial Commit

---
+++ whale/whale.js
@@ -0,0 +1,22 @@
+const input = 'whales are adorable!';
+const vowels = ['a','e','i','o','u'];
+let resultArray = [];
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++){
+    if ( input[inputIndex] === 'e'){
+        resultArray.push(input[inputIndex]);
+    }
+    if (input [inputIndex] === 'u'){
+        resultArray.push(input[inputIndex]);
+    }
+    for ( let vowelIndex = 0; vowelIndex < vowels.length; vowelIndex++){
+        if ( input[inputIndex] === vowels[vowelIndex]){
+            resultArray.push(input[inputIndex]);
+        }
+
+
+    };
+};
+
+const resultString = resultArray.join('').toUpperCase();
+console.log( resultString);
